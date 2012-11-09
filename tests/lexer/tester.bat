2ffb4b90a641b384ccc4d1d5de8446c36ac41489
Larkina Olga <larkina.bad@gmail.com>
2012-11-10 08:39:11 +1100

    Change diff to fc

--- tests/lexer/tester.bat
+++ tests/lexer/tester.bat
@@ -1,3 +1,3 @@
 @echo off
 for %%i in (*.in) do java -jar C_Compiler.jar %%i > %%~ni.ans
-for %%i in (*.out) do diff %%i %%~ni.ans && if ERRORLEVEL 0 echo OK
+for %%i in (*.out) do fc %%i %%~ni.ans && if ERRORLEVEL 0 echo OK
