c17e6102b9c61b19044ab6c4fc84f6bab902cf7b
Larkina Olga <larkina.bad@gmail.com>
2012-11-10 08:35:22 +1100

    Add messages and coods to Exceptions

--- src/lexer/LexerException.java
+++ src/lexer/LexerException.java
@@ -3,9 +3,25 @@ package lexer;
 
 public class LexerException extends Exception{
 
-    public class UnclosedCommentException extends LexerException {
-
-    }   
-
+    Integer line, pos;
+    String msg;
+    
+    LexerException(Integer l, Integer p, String m) {
+        line = l;
+        pos = p;
+        msg = m;
+    }
+    
+    LexerException(){
+        line = 0;
+        pos = 0;
+        msg = "";
+    }
+    
+    @Override
+    public String getMessage(){
+        return "Incorrect token on " + line.toString() + " line, " + pos.toString() + " pos: " + msg;
+    }
+    
 }
 
