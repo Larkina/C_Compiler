5bb27103c1ef9a67fbb432ecd0070fb38626c0dc
Larkina Olga <larkina.bad@gmail.com>
2012-12-20 22:21:22 +1100

    Add struct, fix compound stmt

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -70,7 +70,7 @@ public class Parser {
     }
 
     boolean isTypeToken(String s) {
-        return Util.isIn(s, "int", "void", "double", "struct");
+        return Util.isIn(s, "int", "void", "double", "struct", "char", "float");
     }
     
     boolean isDeclarator() {
@@ -409,259 +409,64 @@ public class Parser {
         }
         return l_node;
     }
-    
-    Node parsePoint(int lvl) throws Exception {
-        Node p = new Node(lvl, getToken());
-        popToken();
+        
+    String parseDeclarator(int lvl) throws Exception {
         next();
-        while(getType() == TokenType.STAR) {
-            p.addChild(new Node(lvl + 1, getToken()));
+        String res = "";
+        while (getType() == TokenType.STAR) {
+            res += " pointer to";
             popToken();
-            next();
+            next(); 
         }
-        return p;
-    }
-    
-    Node parseAbstractDeclaratorFactorized(int lvl) throws Exception {
-        Token t = getToken();
-        TokenType type = getType();
-        Node inner = null, decl = null;
-        switch (type) {
-            case L_PARENTHESIS: {
-                 eatToken(type, "Expected (");
-                 if (getType() != TokenType.R_PARENTHESIS) {
-                    inner = parseParameterList(lvl + 1);
-                 }
-                eatToken(TokenType.R_PARENTHESIS, "Expected )");
-                break;               
-            }
-            case L_BRAKET: {
-                eatToken(type, "Expected (");
-                inner = parseConditionalExpr(lvl + 1, true);
-                eatToken(type, "Expected )");               
-            }
-            default: {
-                return null;
-            }
-        }
-        return new Node(lvl, t).addChild(inner).addChild(parseAbstractDeclaratorFactorized(lvl + 1));
-    }
-    
-    Node parseDirectAbstractDeclarator(int lvl) throws Exception {
-        Token t = getToken();
-        TokenType type = getType();
-        Node inner = null;
-        switch (type) {
-            case L_PARENTHESIS: {
-                eatToken(type, "Expected (");
-                if (isAbstractDeclarator()) {
-                    inner = parseAbstractDeclarator(lvl + 1);
-                }
-                next();
-                if (getType() != TokenType.R_PARENTHESIS) {
-                    inner = parseAbstractDeclarator(lvl + 1);
-                }
-                eatToken(TokenType.R_PARENTHESIS, "Expected )");
-                break;
-            }
-            case L_BRAKET: {
-                eatToken(type, "Expected [");
-                inner = parseConditionalExpr(lvl + 1, true);
-                eatToken(TokenType.R_PARENTHESIS, "Expected ]");
-                break;
-            }
-        }
-        return new Node(lvl, t).addChild(inner).addChild(parseAbstractDeclaratorFactorized(lvl + 1));
-    }
-    
-    Node parseAbstractDeclarator(int lvl) throws Exception {
-        next();
-        Node pointer = null, decl = null;
-        if (getType() == TokenType.STAR) {
-            pointer = parsePoint(lvl);
-            if (isAbstractDeclarator()) {
-                decl = parseDirectAbstractDeclarator(lvl + 1);
-                return new Node(lvl, getToken()).addChild(pointer.incLevel()).addChild(decl);
-            }
-            return pointer;
-        }
-        return parseDirectAbstractDeclarator(lvl);
-    }
-    
-    Node parseParameterDeclaration(int lvl) throws Exception {
-        Node decl_spec = parseDeclarationSpecifiers(lvl);
-        Node decl = null;
-        if (isDeclarator()) {
-            decl = parseDirectDeclarator(lvl + 1);
-            return new Node(lvl, getToken()).addChild(decl_spec.incLevel()).addChild(decl);
-        }
-        if (isAbstractDeclarator()) {
-            decl = parseAbstractDeclarator(lvl + 1);
-            return new Node(lvl, getToken()).addChild(decl_spec.incLevel()).addChild(decl);
-        }
-        return decl_spec;
-    }
-    
-    Node parseParameterList(int lvl) throws Exception {
-        Node l_node = parseParameterDeclaration(lvl);
-        next();
-        if (getType() == TokenType.COMMA) {
-            Token t = getToken();
-            popToken();
-            Node r_node = parseParameterList(lvl + 1);
-            return new Node(lvl, t).addChild(l_node.incLevel()).addChild(r_node);
-        }
-        return l_node;
-    }
-    
-    Node parseIdentiferList(int lvl) throws Exception {
-        next();
-        Node l_node = new Node(lvl, getToken());
-        next();
-        if (getType() == TokenType.COMMA) {
-            Token t = getToken();
-            popToken();
-            Node r_node = parseIdentiferList(lvl + 1);
-            return new Node(lvl, t).addChild(l_node.incLevel()).addChild(r_node);
-        }
-        return l_node;
-    }
-    
-    Node parseDirecDeclaratorFactorized(int lvl) throws Exception {
-        TokenType type = getType();
-        Token tok = getToken();
-        Node inner = null;
-        switch (type) {
-            case L_BRAKET: {
-               eatToken(TokenType.L_BRAKET, "Expected (");
-               inner = parseConditionalExpr(lvl + 1, true);
-               eatToken(TokenType.R_BRAKET, "Expected )");
-               break;
-            }
-            case L_PARENTHESIS: {
-                eatToken(TokenType.L_PARENTHESIS, "Expected (");
-                if (getType() == TokenType.VAR) {
-                    inner = parseIdentiferList(lvl + 1);
-                }
-                if (getType() != TokenType.R_PARENTHESIS) {
-                    inner = parseParameterList(lvl + 1);
-                }
-                eatToken(TokenType.R_PARENTHESIS, "Expected )");
-                break;
-            }
-            default: {
-                return null;
-            }
-        }
-        return new Node(lvl, tok).addChild(inner).addChild(parseDirecDeclaratorFactorized(lvl + 1));
-    }
-    
-    Node parseDirectDeclarator(int lvl) throws Exception {
-        Node node = null;
-        Token t = getToken();;
-        if (getType() == TokenType.VAR) {
-            node = new Node(lvl + 1, t);
-        } else {
-            eatToken(TokenType.L_PARENTHESIS, "Expected (");
-            node = parseDeclarator(lvl + 1);
+        String arg = "";
+        if (getType() == TokenType.L_PARENTHESIS) {
+            eatToken(TokenType.L_PARENTHESIS, "");
+            arg = " " + parseInitDeclarator(lvl + 1, "");
             eatToken(TokenType.R_PARENTHESIS, "Expected )");
         }
-        Node fact = parseDirecDeclaratorFactorized(lvl + 1);
-        return new Node(lvl, t).addChild(node).addChild(fact);
-    }
-    
-    Node parseDeclarator(int lvl) throws Exception {
-        next();
-        Node point = null;
-        if (getType() == TokenType.STAR) {
-            point = parsePoint(lvl + 1);
-        }
-        return parseDirectDeclarator(lvl + 1).addChild(point);
-    }
-    
-    Node parseStructDeclarator(int lvl) throws Exception {
-        next();
-        if(isDeclarator()) {
-            Token t = getToken();
-            Node decl = parseDeclarator(lvl + 1);
-            Node expr = parseConditionalExpr(lvl + 1, true);
-            return new Node(lvl, t).addChild(decl).addChild(expr);
-        }
-        return parseConditionalExpr(lvl, false);
-    }
-    
-    Node parseStructDeclaratorList(int lvl) throws Exception {
-        Node l_node = parseStructDeclarator(lvl);
-        next();
-        if (getType() == TokenType.COMMA) {
-            Token t = getToken();
+        String var = "";
+        if (getType() == TokenType.VAR) {
+            var = getToken().getText() + " is";
             popToken();
-            Node r_node = parseStructDeclaratorList(lvl + 1);
-            return new Node(lvl, t).addChild(l_node.incLevel()).addChild(r_node);
         }
-        return l_node;
-    }
-    
-    Node parseStructDeclaration(int lvl) throws Exception {
-       Token t = getToken();
-       Node type = new Node(lvl + 1, t); 
-       next();
-       while(isTypeToken()) {
-           popToken();
-           type.addChild(parseTypeSpecifier(lvl + 1));
-           next();
-       }
-       Node decl_list = parseStructDeclaratorList(lvl + 1);
-       return new Node(lvl, t).addChild(type).addChild(decl_list);
-    }
-    
-    Node parseStructDeclList(int lvl) throws Exception {
-        Node str_decl = new Node(lvl, getToken());
-        next();
-        while (isTypeToken()) {
-            popToken();
-            str_decl.addChild(parseStructDeclaration(lvl + 1));
-            next();
-        }
-        return str_decl;
-    }
-    
-    Node parseStructSpecifier(int lvl) throws Exception{
-        eatToken("struct", "Expected struct key word");
-        if (getToken().getType() == TokenType.VAR) {
-            // add to symbol table, check existense;
-            Node var = new Node(lvl, getToken());
+        String arr = "";
+        while (getType() == TokenType.L_BRAKET) {
             popToken();
             next();
-            if (getType() == TokenType.L_BRACE) {
-                var.addChild(parseStructDeclList(lvl + 1));
-                eatToken(TokenType.R_BRACE, "Expected }");
+            String add = "";
+            if (getType() != TokenType.R_BRAKET) {
+                add = parseConditionalExpr(lvl, false).toString();
             }
-            return var;
-        } 
-        eatToken(TokenType.L_BRACE, "Expected {");
-        Node st = parseStructDeclList(lvl + 1);
-        eatToken(TokenType.R_BRACE, "Expected }");
-        return st;
-    }
-    
-    Node parseTypeSpecifier(int lvl) throws Exception {
-        if (Util.isIn(getToken().getText(), "void", "int", "double")) {
-            Token t = getToken();
-            return new Node(lvl, t);
-        }
-        return parseStructSpecifier(lvl);
-    }
-    
-    Node parseDeclarationSpecifiers(int lvl) throws Exception {
-        Node type = new Node(lvl, getToken());
-        next();
-        while(isTypeToken(getToken().getText())) {
-            type.addChild(parseTypeSpecifier(lvl + 1));
             popToken();
-            next();
+            arr = "\n" + add + "array of" + arr;
         }
-        return type;
+        return var + arg + arr + res;
+    }
+    
+    String parseStructSpecifier(int lvl) throws Exception {
+        eatToken("struct", ":P");
+        String res = "struct ";
+        if(getType() == TokenType.VAR) {
+            res = getToken().getText() + " " + res;
+            popToken();
+        }
+        else {
+            res = "anonymous " + res;
+        }
+        eatToken(TokenType.L_BRACE, "Expected {");
+        res += "with members { \n" + parseDeclarationList(lvl) + "\n}";
+        eatToken(TokenType.R_BRACE, "Expected }");
+        return res;
+    }
+    
+    String parseDeclarationSpecifiers(int lvl) throws Exception {
+        next();
+        if ("struct".equals(getToken().getText())) {
+            return parseStructSpecifier(lvl);
+        }
+        String res = getToken().getText();
+        popToken();
+        return res;
     }
     
     Node parseInitializerList(int lvl) throws Exception {
@@ -690,45 +495,75 @@ public class Parser {
         return parseAssignExpr(lvl);
     }
     
-    Node parseInitDeclarator(int lvl) throws Exception {
-        Node l_node = parseDeclarator(lvl);
-        next();
-        if (getType() == TokenType.ASSIGN) {
-            Token t = getToken();
+    String parseInitDeclarator(int lvl, String type) throws Exception {
+        String res = "";
+        while (getType() == TokenType.STAR) {
+            res += "pointer to ";
             popToken();
-            Node r_node = parseInitializer(lvl +  1);
-            return new AssignOpNode(lvl, t, l_node.incLevel(), r_node);
+            next();
+        }       
+        String l_node = parseDeclarator(lvl);
+        next();
+        String arg = "";
+        if (getType() == TokenType.L_PARENTHESIS) {
+            eatToken(TokenType.L_PARENTHESIS, "e");
+            int count = 0;
+            next();
+            while(isTypeToken()) {
+                ++count;
+                String t = getToken().getText();
+                popToken();
+                arg += (count > 1 ? ", " : "") + parseInitDeclarator(lvl, t);
+                next();
+                if (getType() == TokenType.COMMA) {
+                    popToken();
+                    next();
+                }
+            }
+            if (count > 0) {
+                arg = "passing " + arg;
+            }
+            arg = "function " + arg + "returning ";
+            eatToken(TokenType.R_PARENTHESIS, "expected )");
         }
-        return l_node;
+        
+        if (getType() == TokenType.ASSIGN) {
+            popToken();
+            Node r_node = parseInitializer(0);
+            return l_node + " " + arg + res + type + " and init with\n" +  r_node.toString();
+        }
+        return l_node + " " + arg + res + type + " ";
     }
     
-    Node parseInitDeclaratorlList(int lvl) throws Exception {
-        Node l_node = parseInitDeclarator(lvl);
+    String parseInitDeclaratorlList(int lvl, String type) throws Exception {
+        String l_node = parseInitDeclarator(lvl, type);
         next();
         if (getType() == TokenType.COMMA) {
             Token t = getToken();
             popToken();
-            Node r_node = parseInitDeclaratorlList(lvl + 1);
-            return new Node(lvl, t).addChild(l_node.incLevel()).addChild(r_node);
+            String r_node = parseInitDeclaratorlList(lvl + 1, type);
+            return l_node + "\n" + r_node;
         }
         return l_node;
     }
     
-    Node parseDeclaration(int lvl) throws Exception {
-        Node l_node = parseDeclarationSpecifiers(lvl);
+    String parseDeclaration(int lvl, boolean should_be_semicolon) throws Exception {
+        String l_node = parseDeclarationSpecifiers(lvl);
         next();
         if (getType() == TokenType.SEMICOLON) {
             popToken();
-            return l_node;
+            return l_node.toString();
         }
-        Node r_node = parseInitDeclaratorlList(lvl + 1);
+        String r_node = parseInitDeclaratorlList(0, l_node);
         next();
-        if (getType() == TokenType.SEMICOLON) {
-            popToken();
-        } else {
-            throw new ParserException(getToken(), "Forgot ;");
+        if (should_be_semicolon) {
+            if (getType() != TokenType.SEMICOLON) {
+                throw new ParserException(getToken(), "Forgot ;");
+            } else {
+                popToken();
+            }
         }
-        return new Node(lvl, getToken()).addChild(l_node.incLevel()).addChild(r_node);
+        return r_node;
     }
     
     Node parseTypeName(int lvl) throws Exception {
@@ -740,10 +575,6 @@ public class Parser {
             type.addChild(new Node(lvl, getToken()));
             popToken();
             next();
-        }
-        if (isAbstractDeclarator()) {
-            Node decl = parseAbstractDeclarator(lvl + 1);
-            return new Node(lvl, getToken()).addChild(type.incLevel()).addChild(decl);
         }
         return type;
     }
@@ -796,16 +627,20 @@ public class Parser {
         if (getType() == TokenType.R_BRACE) {
             return null;
         }
-        Node decl = null;
+        String decl = "";
         if (isTypeToken(curr.getText())) {
-            decl = parseDeclaration(lvl + 1);
+            decl = parseDeclaration(lvl, true);
         }
         if (getType() == TokenType.R_BRACE) {
-            return decl;
+            return null;
         }
-        Node stmt = parseStatement(lvl + 1);
+        Node stmt = new Node(lvl, curr);
+        while (getType() != TokenType.R_BRACE) {
+            stmt.addChild(parseStatement(lvl));
+            next();
+        }
         eatToken(TokenType.R_BRACE, "Expected }");
-        return new CompoundStmtNode(lvl, curr, decl, stmt);
+        return new CompoundStmtNode(lvl, curr, null, stmt);
     }
     
     Node parseExprStatement(int lvl) throws Exception {
@@ -885,49 +720,46 @@ public class Parser {
         return null;
     }
     
-    Node parseDeclarationList(int lvl) throws Exception {
-        Node decl = parseDeclaration(lvl);
+    String parseDeclarationList(int lvl) throws Exception {
+        String decl = parseDeclaration(lvl, true);
         next();
         while (isTypeToken()) {
-            decl.addChild(parseDeclaration(lvl));
+            decl += "\n" + parseDeclaration(lvl, true);
         }
-        return decl;
-    }
-    
-    Node parseFunctionDefinition(int lvl) throws Exception {
-        next();
-        Token t = getToken();
-        Node spec = null, decl = null, decl_list = null, stmt;
-        if (isTypeToken()) {
-            spec = parseDeclarationSpecifiers(lvl + 1);
-        }
-        if (isDeclarator()) {
-            decl = parseDeclarator(lvl + 1);
-        }
-        if (isTypeToken()) {
-            decl_list = parseDeclarationList(lvl + 1);
-        }
-        stmt = parseCompoundStatement(lvl);
-        return new FunctionNode(lvl, t, spec, decl, decl_list, stmt);
+        return decl;        
     }
     
     Node parseExternalDeclaration(int lvl) throws Exception{
-        if (isTypeToken()) {
-            return parseDeclaration(lvl);
+        next();
+        String res = parseDeclaration(lvl, false);
+        next();
+        Node body = null; 
+        if (getType() == TokenType.L_BRACE) {
+            body = parseCompoundStatement(lvl);
+            if (body != null){
+                res += "with body\n" + body.toString();
+            }
         }
-        return parseFunctionDefinition(lvl);
+        else
+            if (getType() != TokenType.SEMICOLON) {
+                throw new ParserException(getToken(), "Expected ;");
+            } else {
+                eatToken(TokenType.SEMICOLON, "");
+            }
+        System.out.println(res);
+        return body;
     }
      
     Node parseTraslationUnit(int lvl) throws Exception {
         Node decl = parseExternalDeclaration(lvl);
-        while (isFunctionDefinition() || isTypeToken()) {
+        while (isTypeToken()) {
             decl.addChild(parseExternalDeclaration(lvl));
         }
         return decl;
     }
     
     public Node parse() throws Exception {
-        return parseExpr(0);
-        //return parseTraslationUnit(0);
+       // return parseExpr(0);
+        return parseTraslationUnit(0);
     }
 }
